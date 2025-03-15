099c4c0276e4476be91593b59bcca0ba8d78bc70
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-03-15 15:41:50 +0100

    Config

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,9 +171,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
-if config('ENVIRONNEMENT') == 'PROD':
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
